608b6ebb931bb1ab9ba7f692322bd5274d263d3e
Alexander Hardeland <alexanderhardeland@gmail.com>
2022-08-23 18:13:54 +0200

    Working on styling and building a login form

---
+++ scripts/components/displayLogInErrorMessage.js
@@ -0,0 +1,17 @@
+const createSpanElement = function(message, messageStyleClass) {
+  const spanElement = document.createElement('span');
+
+  spanElement.classList.add(messageStyleClass);
+  spanElement.innerText = message;
+  return spanElement;
+};
+
+const displayLogInErrorMessage = function(parentElement, message, messageStyleClass) {
+  parentElement.replaceChildren();
+
+  const spanElement = createSpanElement(message, messageStyleClass);
+  parentElement.classList.add(messageStyleClass);
+
+  parentElement.append(spanElement);
+};
+export default displayLogInErrorMessage; 
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,20 @@
-const formElement = document.querySelector("#login-page-form-container");
+import displayLogInErrorMessage from './components/displayLogInErrorMessage.js';
+
+const formElement = document.querySelector('#login-page-form-container');
+const errorMessageContainer = document.querySelector('.input-section__display-message-container');
+
 
 const validateLogInPageForm = function() {
   event.preventDefault();
 
-  const userName = formElement.username.value;
-  const password = formElement.password.value;
+  //clear input values for spaces
+  const userName = formElement.username.value.trim();
+  const password = formElement.password.value.trim();
 
-  console.log('Username :', userName);
-  console.log('Password :', password);
+  console.log("hello");
+  if(userName.length === 0 || password.length === 0) {
+    displayLogInErrorMessage(errorMessageContainer, 'It works', 'message-fail');
+  }
 };
 
 formElement.addEventListener('submit', validateLogInPageForm);
