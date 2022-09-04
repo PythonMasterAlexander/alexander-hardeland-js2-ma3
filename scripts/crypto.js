258b41401b8e66e718a74e10c7cb6d64e1d924c2
Alexander Hardeland <alexanderhardeland@gmail.com>
2022-09-04 18:44:31 +0200

    Finished the project

--- scripts/crypto.js
+++ scripts/crypto.js
@@ -3,24 +3,11 @@ import { baseUrl, mainSectionCryptoContainer } from './constants/constant.js';
 
 const pageProductsUrl = baseUrl + "cryptos";
 
-//How to get parameters out of url strings.
-//get the querystring on page and we need a ? mark with some text after
-//This is how a querystring migh look like ?id=54&name=ben
 const queryString = document.location.search;
-console.log(queryString);
 
-//To return the property of the querystring we use URLSearchParams, we can then use some of the properties to get the values in the querystring
 const params = new URLSearchParams(queryString);
-console.log(params);
 
-//Use the get("id") to get the id from the querystring
-console.log(params.get("id"));
-console.log(params.get("name"));
-
-//use  Immediately Invoked Function Expression to call async function at once
 (async function() {
-
-  //Use a try catch block to catch error
   try {
     const response = await fetch(pageProductsUrl );
     const json = await response.json();
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ import displayLogInErrorMessage from './components/displayLogInErrorMessage.js';
 const formElement = document.querySelector('#login-page-form-container');
 const errorMessageContainer = document.querySelector('.input-section__display-message-container');
 
+const userNameAndPasswordLength = 2;
 
 const validateLogInPageForm = function() {
   event.preventDefault();
@@ -11,9 +12,10 @@ const validateLogInPageForm = function() {
   const userName = formElement.username.value.trim();
   const password = formElement.password.value.trim();
 
-  console.log("hello");
-  if(userName.length === 0 || password.length === 0) {
-    displayLogInErrorMessage(errorMessageContainer, 'It works', 'message-fail');
+  errorMessageContainer.replaceChildren();
+
+  if (userName.length <= userNameAndPasswordLength || password.length <= userNameAndPasswordLength) {
+    return displayLogInErrorMessage(errorMessageContainer, 'Invalid username or password', 'message-fail');
   }
 };
 
